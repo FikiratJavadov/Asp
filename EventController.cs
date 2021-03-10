d8f6ca22e8cdd79c0c03814caf7858393f2227f1
Fikirat Javadov <fikrataj@code.edu.az>
2021-03-10 22:55:56 +0400

    event crud

---
+++ EventController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProjectasp.DAL;
+using FinalProjectasp.Models;
+using FinalProjectasp.ViewModels;
+using Fiorella.Extentions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProjectasp.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class EventController : Controller
+    {
+
+
+
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public EventController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Events.Where(e => e.IsDeleted == false).ToList());
+        }
+
+
+        public async Task<IActionResult> Create() {
+            ViewBag.Categories = await _context.Categories.Where(c => c.IsDeleted == false).ToListAsync();
+            ViewBag.Speaker = await _context.Speakers.ToListAsync();
+
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(EventVm e, int eventCategory, int eventSpeaker) // VM => Evenet, Detail, Category, Speaker
+        {
+            ViewBag.Categories = await _context.Categories.Where(c => c.IsDeleted == false).ToListAsync();
+            ViewBag.Speaker = await _context.Speakers.ToListAsync();
+
+            //if (!e.Photo.IsSelectedType("image/")){
+            //    ModelState.AddModelError("Photo", "Not image type");
+            //    return View();
+            //};
+
+
+            string path = Path.Combine("img", "event");
+
+
+            Event newEvent = new Event
+            {
+                Day = e.Day,
+                StartTime = e.StartTime,
+                Endtime = e.Endtime,
+                Location = e.Location,
+                EventName = e.EventName,
+                CategoryId = eventCategory,
+                IsDeleted = false,
+                Image = await e.Photo.SaveImageAsync(_env.WebRootPath, path)
+
+               
+            };
+
+            await _context.Events.AddAsync(newEvent);
+            await _context.SaveChangesAsync();
+
+
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+    } 
+
+
---
+++ EventVm.cs
@@ -0,0 +1,43 @@
+﻿using FinalProjectasp.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectasp.ViewModels
+{
+    public class EventVm
+    {
+
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public DateTime Day { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime Endtime { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string EventName { get; set; }
+
+
+        [Required]
+
+        public bool IsDeleted { get; set; }
+        [Required]
+
+
+
+        public string Description { get; set; }
+
+
+
+        public IFormFile Photo { get; set; }
+    }
+}
+
